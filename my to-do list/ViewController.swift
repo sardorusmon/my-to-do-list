94ff97c9823c83b0fa826f7303f335993226c05a
SardorUsmon <sardorusmon111@gmail.com>
2022-11-19 22:02:33 +0500

    Initial Commit

---
+++ my to-do list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my to-do list
+//
+//  Created by Sardor Panjiyev on 19/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
